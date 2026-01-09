a2a6d10232c5cb9e77687729fb56bb3cd5849c00
sofishuranova <51199618+sofishuranova@users.noreply.github.com>
2026-01-09 12:53:15 +0200

    add test for Hero section on Main page

--- cypress/e2e/go2che.cy.js
+++ cypress/e2e/go2che.cy.js
@@ -3,4 +3,33 @@ describe('Main page', () => {
     cy.visit('/')
     cy.url().should('include', 'go2che.in.ua')
   })
+  
+  it('Displays slider on the Hero section', () => {
+    cy.visit('/')
+    cy.get('.images-section__slider', { timeout: 10000 }).should('be.visible')
+  })
+
+  it('Displays title on the Hero section', () => {
+    cy.visit('/')
+    cy.get('.images-section__content_title', { timeout: 10000 }).should('be.visible')
+  })
+
+  it('CTA button is visible on the Hero section', () => {
+    cy.visit('/')
+    cy.contains('Приєднайся', { timeout: 10000 }).as('cta').should('be.visible')
+
+    // Check button has non-empty text
+    cy.get('@cta').invoke('text').should('not.be.empty')
+
+    // Check button contains an icon element (svg, <i>, or common icon classes)
+    cy.get('@cta').find('.icon-arrow-right').should('exist')
+  })
+
+  it('CTA link points to Become Partner page and opens in the same tab', () => {
+  cy.visit('/')
+  cy.contains('Приєднайся', { timeout: 10000 })
+    .first()
+    .should('have.attr', 'href', 'https://go2che.in.ua/become-partner/')
+    .and('have.attr', 'target', '_self')
+  })
 })
