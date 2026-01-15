db06883fa97803c7263360900e5f90ed7fcfd8e4
sofishuranova <51199618+sofishuranova@users.noreply.github.com>
2026-01-15 23:59:44 +0200

    add archive folder

--- cypress.config.js
+++ cypress.config.js
@@ -3,5 +3,6 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://go2che.in.ua',
+    excludeSpecPattern: ['cypress/e2e/_archive/**/*'],
   },
 })
