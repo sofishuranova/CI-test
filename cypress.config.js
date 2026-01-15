6519fda5d46fa0da4fa17a283c85ba9ccd8cc8ba
sofishuranova <51199618+sofishuranova@users.noreply.github.com>
2026-01-15 23:20:58 +0200

    update config

--- cypress.config.js
+++ cypress.config.js
@@ -3,18 +3,5 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://go2che.in.ua',
-
-    setupNodeEvents(on, config) {
-      on('before:browser:launch', (browser, launchOptions) => {
-        if (browser.name === 'chrome' && browser.isHeadless) {
-          launchOptions.args.push('--no-sandbox')
-          launchOptions.args.push('--disable-gpu')
-          launchOptions.args.push('--disable-dev-shm-usage')
-          launchOptions.args.push('--remote-debugging-port=9222') 
-        }
-
-        return launchOptions
-      })
-    },
   },
 })
